20ebce5b41616cf4ea57ce9c1033da8f79c90f99
Sam Martin <samjackmartin@gmail.com>
2021-12-08 21:03:34 +0000

    Contains for resources

--- policyglass/resource.py
+++ policyglass/resource.py
@@ -1,5 +1,19 @@
 """Resource class."""
+from fnmatch import fnmatch
 
 
 class Resource(str):
     """A resource ARN may be case sensitive or case insensitive depending on the resource type."""
+
+    def __contains__(self, other: object) -> bool:
+        """Whether this object contains (but is not equal to) another object.
+
+        Parameters:
+            other: The object to determine if our object contains.
+
+        Raises:
+            ValueError: If the other object is not of the same type as this object.
+        """
+        if not isinstance(other, self.__class__):
+            raise ValueError(f"Cannot compare {self.__class__.__name__} and {other.__class__.__name__}")
+        return self != other and fnmatch(other, self)
---
+++ tests/unit/contains/test_resource.py
@@ -0,0 +1,24 @@
+import pytest
+
+from policyglass import Resource
+
+RESOURCE_IN_SCENARIOS = {
+    "smaller": ["arn:aws:ec2:*:*:volume/vol-12345678", "arn:aws:ec2:*:*:volume/*"],
+}
+
+
+@pytest.mark.parametrize("_, scenario", RESOURCE_IN_SCENARIOS.items())
+def test_resource_contains(_, scenario):
+    assert Resource(scenario[0]) in Resource(scenario[1])
+
+
+RESOURCE_NOT_IN_SCENARIOS = {
+    "exactly_equal": ["arn:aws:ec2:*:*:volume/*", "arn:aws:ec2:*:*:volume/*"],
+    "case_unequal": ["arn:aws:ec2:*:*Volume/*", "arn:aws:ec2:*:*:volume/*"],
+    "larger": ["arn:aws:ec2:*:*:volume/*", "arn:aws:ec2:*:*:volume/vol-12345678"],
+}
+
+
+@pytest.mark.parametrize("_, scenario", RESOURCE_NOT_IN_SCENARIOS.items())
+def test_resource_not_contains(_, scenario):
+    assert Resource(scenario[0]) not in Resource(scenario[1])
