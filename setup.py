2bd986f8415693fee8e4370dd716fbf313468b78
Sam Martin <samjackmartin@gmail.com>
2021-12-12 15:18:03 +0000

    Fix tests

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(this_directory, "README.rst"), encoding="utf-8") as f:
     long_description = re.sub(r"..\s+doctest\s+::", ".. code-block ::", f.read())
 
 setup(
-    version="0.1.0",
+    version="0.1.1",
     python_requires=">=3.6.0",
     name="policyglass",
     packages=find_packages(include=["policyglass", "policyglass.*"]),
@@ -20,7 +20,7 @@ setup(
     author="Sam Martin",
     author_email="samjackmartin+policyglass@gmail.com",
     url="https://github.com/CloudWanderer-io/PolicyGlass",
-    install_requires=["pydantic"],
+    install_requires=["pydantic", 'typing_extensions; python_version < "3.8.0"'],
     package_data={
         "": ["py.typed"],
     },
